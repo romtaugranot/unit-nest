18d37cb8af5deb77aad4d3ac797c7778c4578373
Rom Granot <romgranot@mail.tau.ac.il>
2025-09-20 14:27:48 +0300

    feat: add test module compiling

--- lib/public/builders/tests.builder.ts
+++ lib/public/builders/tests.builder.ts
@@ -1,3 +1,4 @@
+import { Test, TestingModule } from '@nestjs/testing';
 import { MethodKeys, Provider, TestSuiteStore } from '../../private';
 import { SuiteBuilder } from './suite.builder';
 
@@ -5,6 +6,9 @@ export class TestsBuilder<S extends Provider> {
   private readonly providers: Provider[];
   private readonly cut: S;
   private readonly suiteStore: TestSuiteStore<S, MethodKeys<S>>;
+
+  private testingModule: TestingModule | null = null;
+  private _cutInstance: InstanceType<S> | null = null;
 
   constructor(cut: S, ...providers: Provider[]) {
     this.providers = providers;
@@ -19,5 +23,36 @@ export class TestsBuilder<S extends Provider> {
     return new SuiteBuilder<S, MethodKeys<S>>(method, this, this.suiteStore);
   }
 
-  run(): void {}
+  /**
+   * Create and compile the NestJS TestingModule
+   */
+  private async createTestingModule(): Promise<void> {
+    const moduleBuilder = Test.createTestingModule({
+      providers: [this.cut, ...this.providers],
+    });
+
+    this.testingModule = await moduleBuilder.compile();
+
+    this._cutInstance = this.testingModule.get<InstanceType<S>>(this.cut);
+  }
+
+  /**
+   * Get the compiled CUT instance
+   */
+  get cutInstance(): InstanceType<S> {
+    if (!this._cutInstance) {
+      throw new Error('TestingModule not compiled. Call run() first.');
+    }
+
+    return this._cutInstance;
+  }
+
+  /**
+   * Run all test suites
+   */
+  async run(): Promise<void> {
+    await this.createTestingModule();
+
+    // TODO: Implement test execution logic
+  }
 }
